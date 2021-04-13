267238eb99e237c46b1ba41aa47a111fd868fc20
Mykyta Nekh <nikita.nekh@gmail.com>
2021-04-13 22:55:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,81 +6,44 @@ class CountdownTimer {
       (this.hours = this.selector.querySelector('[data-value="hours"]')),
       (this.mins = this.selector.querySelector('[data-value="mins"]')),
       (this.secs = this.selector.querySelector('[data-value="secs"]'));
-  }
-}
-const labels = document.querySelectorAll(".label");
-const values = document.querySelectorAll(".value");
-
+          (this.labels = this.selector.querySelectorAll(".label"));
+      (this.values = this.selector.querySelectorAll(".value"));
 setInterval(() => {
   const days = Math.floor(
-    (timer.targetDate - new Date()) / (1000 * 60 * 60 * 24)
+    (this.targetDate - new Date()) / (1000 * 60 * 60 * 24)
   );
 
-  /*
-   * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-   * остатка % и делим его на количество миллисекунд в одном часе
-   * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-   */
-  function hours() {
-    const mathHour = Math.floor(
-      ((timer.targetDate - new Date()) % (1000 * 60 * 60 * 24)) /
-        (1000 * 60 * 60)
-    );
-    if (mathHour < 10) {
-      return "0" + mathHour;
-    } else {
-      return mathHour;
-    }
-  }
 
-  /*
-   * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-   * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-   */
+let hours = Math.floor(((this.targetDate - new Date()) % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  if (hours < 10) {
+    hours = '0' + hours;
+}
 
-  function mins() {
-    const mathMins = Math.floor(
-      ((timer.targetDate - new Date()) % (1000 * 60 * 60)) / (1000 * 60)
-    );
-    if (mathMins < 10) {
-      return "0" + mathMins;
-    } else {
-      return mathMins;
-    }
-  }
 
-  /*
-   * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-   * миллисекунд в одной секунде (1000)
-   */
+let mins = Math.floor(((this.targetDate - new Date()) % (1000 * 60 * 60)) / (1000 * 60));
+  if (mins < 10) {
+    mins = '0' + mins;
+}
 
-  function secs() {
-    const mathSecs = Math.floor(
-      ((timer.targetDate - new Date()) % (1000 * 60)) / 1000
-    );
-    if (mathSecs < 10) {
-      return "0" + mathSecs;
-    } else {
-      return mathSecs;
-    }
-  }
-  timer.days.textContent = days;
-  timer.hours.textContent = hours();
-  timer.mins.textContent = mins();
-  timer.secs.textContent = secs();
+  let secs = Math.floor(((this.targetDate - new Date()) % (1000 * 60)) / 1000);
+    if (secs < 10) {
+    secs = '0' + secs;
+}
+
+  this.days.textContent = days;
+  this.hours.textContent = hours;
+  this.mins.textContent = mins;
+  this.secs.textContent = secs;
 }, 1000);
+  }
+}
 
 const timer = new CountdownTimer("#timer-1", "March 2, 2024");
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
 
-labels.forEach((label) => (label.style.display = "none"));
-values.forEach((value) => {
+timer.labels.forEach((label) => (label.style.display = "none"));
+timer.values.forEach((value) => {
   value.style.margin = "20px";
   value.style.fontWeight = 700;
   value.style.fontSize = "70px";
 });
 
-timer.selector.style.display = "flex";
